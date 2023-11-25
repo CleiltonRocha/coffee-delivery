ad41acfd13836b7dcb1df0262310ee95fb22d67e
Cleilton Rocha <cleiltonrv30@gmail.com>
2023-11-24 23:18:55 -0300

    fix: coffe list responsivity

--- src/pages/Home/components/CoffeeList/styles.ts
+++ src/pages/Home/components/CoffeeList/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const CoffeeMenu = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 16rem);
+  grid-template-columns: repeat(4, 1fr);
   grid-template-rows: 4;
   column-gap: 2rem;
   row-gap: 2.5rem;
